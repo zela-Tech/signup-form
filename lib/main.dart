f6bb06b9ecbedd9c142a63f1583a0993debf72a2
Adom Z <zelalemadom@gmail.com>
2026-03-25 18:18:06 -0400

    feat: add welcome screen that uses navigator push on successful signup

--- lib/main.dart
+++ lib/main.dart
@@ -166,3 +166,47 @@ class _SignupPageState extends State<SignupPage> {
     );
   }
 }
+//New Screen that's shown after successful signup
+class WelcomeScreen extends StatelessWidget {
+  final String name; //receves the users name as a parameter
+
+  const WelcomeScreen({super.key, required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple.shade50,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Icon(Icons.check_circle, color: Colors.green,size: 80),
+            const SizedBox(height: 20),
+            Text(
+              'Welcome, $name!',
+              style: const TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                color: Colors.purple,
+              ),
+            ),
+            const SizedBox(height: 12),
+            const Text(
+              'Your account has been created.',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+            const SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: () {
+                //go back to signup
+                Navigator.pop(context);
+              },
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.purple,),
+              child: const Text('Back to Sign In'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
